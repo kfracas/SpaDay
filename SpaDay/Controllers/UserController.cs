c23ebc9499ca1c5582079a1c7423f0bd002126e2
Kirby <kafracas@gmail.com>
2020-11-12 20:47:59 -0600

    completed spa user sign up studio

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            ViewBag.username = "";
+            ViewBag.email = "";
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/User/Add")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            ViewBag.date = newUser.Date;
+            ViewBag.email = newUser.Email;
+
+            if (newUser.Password == verify)
+            {
+                ViewBag.name = newUser.Username;
+                return View("Index");
+            } else
+            {
+                ViewBag.noMatch = true;
+                ViewBag.error = "passwords do not match";
+                return View("Add");
+            }
+
+        }
+    }
+}
